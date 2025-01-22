d0ec55146733e493d535e25826b631bb530d9b53
Spandan Bera <spandanbera1710@gmail.com>
2025-01-22 11:42:51 +0530

    Update tailwind.config.js, index.css, App.js, and package.json

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Giant Coffee clone</h1>
+      <h1 className='text-3xl font-bold underline'>Giant Coffee clone</h1>
     </div>
   );
 }
